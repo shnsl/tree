a8e4b4a1e53ad189934e89f22444bf29bf4bc232
shnsl <omerhilalpt@gmail.com>
2026-08-31 00:31:56 +0300

    Add maskable PWA icons and update app shell

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,9 @@ const APP_SHELL = [
   './index.html',
   './manifest.webmanifest',
   './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icons/icon-512.png',
+  './icons/icon-maskable-192.png',
+  './icons/icon-maskable-512.png'
 ];
 
 self.addEventListener('install', (event) => {
