625cbef9904602fdf9ea855c8887ac1792dd2186
shnsl <omerhilalpt@gmail.com>
2026-09-13 13:12:29 +0300

    zz

--- app-config.js
+++ app-config.js
@@ -19,7 +19,7 @@
 // global nesneyi temsil eder; böylece tek dosya her iki ortamda da çalışır.
 self.PWA_CONFIG = {
   SHORT_NAME: 'ARSH',
-  VERSION: '14',
+  VERSION: '15',
   NAME: 'Varlık ve Süreç Yönetim Haritası',
 
   // ================================================================
